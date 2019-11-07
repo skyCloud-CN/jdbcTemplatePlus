22d7890920317b253db5433cccc23801e4a25710
yuntian <yuntian@fenbi.com>
2019-11-07 22:53:33 +0800

    MOD 修复并优化插件

---
+++ fastdao-core/src/main/java/io/github/skycloud/fastdao/core/plugins/autofill/handler/CurrentTimestampAutoFillHandler.java
@@ -0,0 +1,14 @@
+/**
+ * @(#)CurrentTimestampAutoFillHandler.java, 11月 07, 2019.
+ * <p>
+ * Copyright 2019 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package io.github.skycloud.fastdao.core.plugins.autofill.handler;
+
+/**
+ * @author yuntian
+ */
+public class CurrentTimestampAutoFillHandler {
+
+}
